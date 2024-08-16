dea68ba35e980c92262e6ca29b94c4a231331616
injuk <ingnoh1001@gmail.com>
2024-08-16 20:09:55 +0900

    feat: reactive neo4j 설정

---
+++ src/main/kotlin/ga/injuk/graphfs/application/configuration/GraphfsConfiguration.kt
@@ -0,0 +1,24 @@
+package ga.injuk.graphfs.application.configuration
+
+import org.neo4j.cypherdsl.core.renderer.Dialect
+import org.neo4j.driver.Driver
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.neo4j.core.ReactiveDatabaseSelectionProvider
+import org.springframework.data.neo4j.core.transaction.ReactiveNeo4jTransactionManager
+import org.neo4j.cypherdsl.core.renderer.Configuration as Neo4JConfiguration
+
+@Configuration
+class GraphfsConfiguration {
+
+    @Bean
+    fun cypherDslConfiguration(): Neo4JConfiguration = Neo4JConfiguration.newConfig().run {
+        withDialect(Dialect.NEO4J_5)
+        build()
+    }
+
+    @Bean
+    fun reactiveTransactionManager(driver: Driver, databaseNameProvider: ReactiveDatabaseSelectionProvider): ReactiveNeo4jTransactionManager {
+        return ReactiveNeo4jTransactionManager(driver, databaseNameProvider)
+    }
+}
