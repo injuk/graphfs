1392eeabab88044a9474c07233ef2bfb1b83a105
injuk <ingnoh1001@gmail.com>
2024-08-16 19:35:02 +0900

    init: 프로젝트 스캐폴딩

---
+++ src/main/kotlin/ga/injuk/graphfs/GraphfsApplication.kt
@@ -0,0 +1,11 @@
+package ga.injuk.graphfs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphfsApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphfsApplication>(*args)
+}
---
+++ src/test/kotlin/ga/injuk/graphfs/GraphfsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ga.injuk.graphfs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphfsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
