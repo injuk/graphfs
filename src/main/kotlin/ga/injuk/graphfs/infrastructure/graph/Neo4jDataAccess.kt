a9a24c308ac4cba17ba4aa5120e48dd9608d8bc9
injuk <ingnoh1001@gmail.com>
2024-08-23 19:51:50 +0900

    feat: Id 기반 폴더 조회 쿼리 수정

--- src/main/kotlin/ga/injuk/graphfs/infrastructure/graph/Neo4jDataAccess.kt
+++ src/main/kotlin/ga/injuk/graphfs/infrastructure/graph/Neo4jDataAccess.kt
@@ -6,6 +6,6 @@ import org.springframework.data.neo4j.repository.query.Query
 import reactor.core.publisher.Mono
 
 interface Neo4jDataAccess : ReactiveNeo4jRepository<Folder, String> {
-    @Query("MATCH (root: Folder)-[:DIRECT_CHILDREN*]->(f: Folder) WHERE root.id = :rootId AND f.id = :id RETURN f LIMIT 1")
+    @Query("MATCH (root: Folder)-[:DIRECT_CHILDREN*]->(f: Folder) WHERE root.id = \$rootId AND f.id = \$id RETURN f LIMIT 1")
     fun findOneById(rootId: String, id: String): Mono<Folder?>
 }
