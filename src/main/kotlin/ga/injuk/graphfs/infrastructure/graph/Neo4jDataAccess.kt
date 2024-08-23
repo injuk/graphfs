26a35778afe69c364c721ed50756d5f76cf03845
injuk <ingnoh1001@gmail.com>
2024-08-23 19:46:40 +0900

    feat: 루트 기반 조회 쿼리 작성

--- src/main/kotlin/ga/injuk/graphfs/infrastructure/graph/Neo4jDataAccess.kt
+++ src/main/kotlin/ga/injuk/graphfs/infrastructure/graph/Neo4jDataAccess.kt
@@ -2,8 +2,10 @@ package ga.injuk.graphfs.infrastructure.graph
 
 import ga.injuk.graphfs.domain.Folder
 import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository
+import org.springframework.data.neo4j.repository.query.Query
 import reactor.core.publisher.Mono
 
 interface Neo4jDataAccess : ReactiveNeo4jRepository<Folder, String> {
-    fun findOneByName(name: String): Mono<Folder?>
+    @Query("MATCH (root: Folder)-[:DIRECT_CHILDREN*]->(f: Folder) WHERE root.id = :rootId AND f.id = :id RETURN f LIMIT 1")
+    fun findOneById(rootId: String, id: String): Mono<Folder?>
 }
