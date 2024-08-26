559025ef33a161d22f0decd29304343e86e00b5a
injuk <ingnoh1001@gmail.com>
2024-08-26 19:23:00 +0900

    feat: Drive 단건 조회 쿼리 작성

--- src/main/kotlin/ga/injuk/graphfs/infrastructure/graph/DrivesDataAccess.kt
+++ src/main/kotlin/ga/injuk/graphfs/infrastructure/graph/DrivesDataAccess.kt
@@ -2,6 +2,10 @@ package ga.injuk.graphfs.infrastructure.graph
 
 import ga.injuk.graphfs.domain.Drive
 import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository
+import org.springframework.data.neo4j.repository.query.Query
+import reactor.core.publisher.Mono
 
 interface DrivesDataAccess : ReactiveNeo4jRepository<Drive, String> {
+    @Query("MATCH (d: Drive) WHERE d.projectId = \$projectId AND d.id = \$driveId RETURN d")
+    fun findByProjectIdAndId(projectId: String, driveId: String): Mono<Drive>
 }
