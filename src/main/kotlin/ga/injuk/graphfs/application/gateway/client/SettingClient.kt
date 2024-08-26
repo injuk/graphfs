17c1abf07123d0d4fc391ca6f9171848d0e39543
injuk <ingnoh1001@gmail.com>
2024-08-26 18:55:44 +0900

    feat: SettingClient 메소드 시그니쳐 수정

--- src/main/kotlin/ga/injuk/graphfs/application/gateway/client/SettingClient.kt
+++ src/main/kotlin/ga/injuk/graphfs/application/gateway/client/SettingClient.kt
@@ -1,8 +1,8 @@
 package ga.injuk.graphfs.application.gateway.client
 
+import ga.injuk.graphfs.domain.Drive
 import ga.injuk.graphfs.domain.Project
-import ga.injuk.graphfs.domain.RootFolder
 
 interface SettingClient {
-    suspend fun getRootFolder(domain: String, project: Project): RootFolder?
+    suspend fun getDrive(project: Project, driveId: String): Drive?
 }
