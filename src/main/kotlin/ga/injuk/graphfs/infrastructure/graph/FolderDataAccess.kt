ff73d704c72ce207c73fc0f4003222b70a8f3c6a
injuk <ingnoh1001@gmail.com>
2024-08-27 19:43:27 +0900

    feat: 드라이브 삭제 유즈케이스를 위한 쿼리 추가

--- src/main/kotlin/ga/injuk/graphfs/infrastructure/graph/FolderDataAccess.kt
+++ src/main/kotlin/ga/injuk/graphfs/infrastructure/graph/FolderDataAccess.kt
@@ -3,9 +3,13 @@ package ga.injuk.graphfs.infrastructure.graph
 import ga.injuk.graphfs.domain.Folder
 import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository
 import org.springframework.data.neo4j.repository.query.Query
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface FolderDataAccess : ReactiveNeo4jRepository<Folder, String> {
     @Query("MATCH (drive: Drive)-[:DIRECT_CHILD*]->(folder: Folder) WHERE drive.id = \$driveId AND folder.id = \$id RETURN folder LIMIT 1")
     fun findByDriveAndId(driveId: String, id: String): Mono<Folder?>
+
+    @Query("MATCH (drive: Drive)-[:DIRECT_CHILD]->(roots: Folder) WHERE drive.projectId = \$projectId AND drive.id = \$driveId return roots")
+    fun findRootsByProjectIdAndDriveId(projectId: String, driveId: String): Flux<Folder>
 }
