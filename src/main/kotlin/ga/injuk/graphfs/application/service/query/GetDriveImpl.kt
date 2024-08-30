3b299c1e223d4e4014b276834e8a27845978f3f2
injuk <ingnoh1001@gmail.com>
2024-08-30 19:09:44 +0900

    feat: 드라이브 상세 조회시 root 폴더 목록을 함께 반환하도록 개선

--- src/main/kotlin/ga/injuk/graphfs/application/service/query/GetDriveImpl.kt
+++ src/main/kotlin/ga/injuk/graphfs/application/service/query/GetDriveImpl.kt
@@ -1,18 +1,26 @@
 package ga.injuk.graphfs.application.service.query
 
+import ga.injuk.graphfs.application.ReactiveExtension.toList
 import ga.injuk.graphfs.domain.Drive
 import ga.injuk.graphfs.domain.User
 import ga.injuk.graphfs.domain.useCase.GetDrive
 import ga.injuk.graphfs.infrastructure.graph.DriveDataAccess
+import ga.injuk.graphfs.infrastructure.graph.FolderDataAccess
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class GetDriveImpl(
     private val driveDataAccess: DriveDataAccess,
+    private val folderDataAccess: FolderDataAccess,
 ) : GetDrive {
     override suspend fun execute(user: User, request: GetDrive.Request): Drive {
-        return driveDataAccess.findByProjectIdAndId(user.project.id, request.id).awaitSingleOrNull()
+        val drive = driveDataAccess.findByProjectIdAndId(user.project.id, request.id).awaitSingleOrNull()
             ?: throw RuntimeException("there is no drive(${request.id}) in project")
+
+        val roots = folderDataAccess.findRootsByProjectIdAndDriveId(user.project.id, drive.id)
+            .toList()
+
+        return drive.copy(children = roots)
     }
 }
