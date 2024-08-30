cffce765060e54063c4c38c728bcf8e2fc951554
injuk <ingnoh1001@gmail.com>
2024-08-30 18:45:26 +0900

    feat: 리소스와 폴더의 관계 방향을 역전

--- src/main/kotlin/ga/injuk/graphfs/domain/Resource.kt
+++ src/main/kotlin/ga/injuk/graphfs/domain/Resource.kt
@@ -2,9 +2,13 @@ package ga.injuk.graphfs.domain
 
 import org.springframework.data.neo4j.core.schema.Id
 import org.springframework.data.neo4j.core.schema.Node
+import org.springframework.data.neo4j.core.schema.Relationship
 
 @Node
 data class Resource(
     @Id
     val id: String,
+
+    @Relationship(type = "RESOURCE_OF", direction = Relationship.Direction.OUTGOING)
+    val folder: Folder,
 )
