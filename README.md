e9d9302e8c759482fa6eb9ee3680dc20805ae1d1
injuk <ingnoh1001@gmail.com>
2024-09-01 19:32:39 +0900

    chore: 주석 추가

--- README.md
+++ README.md
@@ -41,5 +41,5 @@ graphfs
   - 이건 resource 목록 조회 API로 해결하는게 맞다고 봄
 - [x] folder 삭제 API가 resource 존재 여부를 함께 체크하도록 개선
 - [x] folder 이름 기반 조회 API 개발
-- [ ] folder 조상 조회(?) API 개발
+- [x] folder 조상 조회 API 개발
 - [ ] folder 이동 API 개발
--- src/main/kotlin/ga/injuk/graphfs/domain/useCase/folder/ListElderFolders.kt
+++ src/main/kotlin/ga/injuk/graphfs/domain/useCase/folder/ListElderFolders.kt
@@ -3,6 +3,13 @@ package ga.injuk.graphfs.domain.useCase.folder
 import ga.injuk.graphfs.domain.Folder
 import ga.injuk.graphfs.domain.useCase.UseCase
 
+/**
+ * 파일시스템에서 1과 2이라는 최상위 폴더가 있고, 1은 자식 폴더 1-1, 1-2, 1-3 / 2는 자식 폴더 2-1을 갖는다고 하자.
+ * 또한, 1-2는 다시 자식 폴더인 1-2-1과 1-2-2를 갖고 2-1은 2-1-1, 2-1-2, 2-1-3을 갖는다.
+ * 이러한 구조를 트리 형태로 보여주는 UI가 있다고 할 때, 사용자가 1-2-1의 정보를 요청한다면
+ * 1-2-1까지의 경로가 마치 열려 있는 것처럼 보여주기 위해 1, 2, 1-1, 1-2, 1-3, 1-2-1, 1-2-2를 반환할 API가 필요하다.
+ * 즉, ListElderFolders 유즈케이스는 임의의 폴더로부터 최상위 조상 폴더까지의 경로 상에 포함된 모든 폴더를 반환한다.
+ */
 interface ListElderFolders : UseCase<ListElderFolders.Request, List<Folder>> {
     override val name: String
         get() = ListElderFolders::class.java.name
